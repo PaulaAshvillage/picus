fc2780188d1fd1721ac18fcb667a2fd1de6efe7c
Paula Aschendorf <paula.aschendorf@gmail.com>
2024-02-05 19:08:27 +0100

    adds showmore btn

--- script.js
+++ script.js
@@ -1,12 +1,7 @@
 const button = document.querySelector("#past-shows");
 const table = document.querySelector("#past-shows-table");
 button.addEventListener("click", function () {
-  if (button.innerText === "show less") {
-    table.classList.add("table-hidden");
-    button.innerText = "show more";
-  } else {
-    button.innerText = "show less";
-    table.classList.remove("table-hidden");
-    table.classList.add("table");
-  }
+  table.classList.remove("table-hidden");
+  table.classList.add("table");
+  button.classList.add("btn-hidden");
 });
